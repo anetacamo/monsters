5401d4eff6a6e7446b456d90f30488afabc94c3c
Aneta <anc@praqma.net>
2020-04-13 19:28:50 +0200

    Updates

---
+++ precache-manifest.333e2667ac69c291b7e2f4cad6d38dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92001fa5bc70415eccd8509fe59d6bc1",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "112240f49c0b5f2a8035",
+    "url": "/monsters/static/css/main.b680dba2.chunk.css"
+  },
+  {
+    "revision": "81dc3df8a76e42da3424",
+    "url": "/monsters/static/js/2.bbf9c22f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.bbf9c22f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "112240f49c0b5f2a8035",
+    "url": "/monsters/static/js/main.4f720bf4.chunk.js"
+  },
+  {
+    "revision": "7d83d1b6fa4d9edf6fcf",
+    "url": "/monsters/static/js/runtime-main.6f135e16.js"
+  }
+]);
